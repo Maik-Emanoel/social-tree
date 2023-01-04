96e3ce916a14900cbf4ce6de56f521938305655c
Maik Emanoel <105499887+maik-emanoel@users.noreply.github.com>
2023-01-04 11:27:36 -0300

    adjust font color on userInstagram name

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const body = document.querySelector('body')
 const userInstagram = document.querySelector('.userInstagram')
 const profilePhoto = document.querySelector('.profilePhoto img')
 
+window.addEventListener('load', () => {
+  body.classList.add('blue')
+  userInstagram.classList.add('blue')
+})
 
 profilePhoto.addEventListener('dblclick', function changeTheme() {
   body.classList.toggle('blue')
   userInstagram.classList.toggle('blue')
 })
-
-
 
